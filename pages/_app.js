9f0500b356b5ae1b72ea93a9be99e6d619a61e23
Reilly <rks5920@psu.edu>
2025-04-18 15:34:09 -0400

    Added an icon

--- pages/_app.js
+++ pages/_app.js
@@ -23,9 +23,9 @@ export default function App({ Component, pageProps }) {
           <title>I-Witness</title>
           <meta name='description' content='Put a description here about your app'/>
           <meta name='robots' content='index, follow'/>
-          <link rel="apple-touch-icon" sizes="180x180" href="/favicon_package/apple-touch-icon.png"/>
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon_package/favicon-32x32.png"/>
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon_package/favicon-16x16.png"/>
+          <link rel="apple-touch-icon" sizes="180x180" href="/logothing.png"/>
+          <link rel="icon" type="image/png" sizes="180x180" href="/logothing.png"/>
+          <link rel="icon" type="image/png" sizes="180x180" href="/logothing.png"/>
           <link rel="manifest" href="/favicon_package/site.webmanifest"/>
           <meta name="msapplication-TileColor" content="#da532c"/>
           <meta name="theme-color" content="#ffffff"/>
