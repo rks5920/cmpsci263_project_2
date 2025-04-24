51c081e1c64c048a80b460553d87a74471e76de6
Reilly <rks5920@psu.edu>
2025-04-24 11:42:54 -0400

    Worked on smart contract

--- public/SmartContract.sol
+++ public/SmartContract.sol
@@ -16,21 +16,36 @@ contract MediatedWager{
             uint256 BetSize;
         }
 
-    struct Account {
-        address User;
-        uint[] Contracts;
-
-    }
-
     address payable public owner;
-    Wager[] public Wagers;
+    mapping(uint => Wager) public Wagers;
+    mapping(address => uint[]) public Contracts;
 
     constructor(){
         owner = payable (msg.sender);
     }
 
-    function initWager(uint256 id, address User2, address Mediator, uint256 BetSize) public payable {
-        Wagers.push(Wager({id: id, User1: msg.sender, User2: User2, Mediator: Mediator, User2Consent: false, MediatorConsent: false, BetSize: BetSize}));
+    function initWager( address User2, address Mediator, uint256 BetSize) public payable {
+        uint id = Wagers.length + 1;
+        Wagers[id] = Wager({id: id, User1: msg.sender, User2: User2, Mediator: Mediator, User2Consent: false, MediatorConsent: false, BetSize: BetSize});
+        Contracts[msg.sender].push(id);
+        Contracts[User2].push(id);
+        Contracts[Mediator].push(id);
+    }
+
+    function acceptWager_User2(uint id) public payable {
+        require(msg.sender == Wagers[id].User2);
+        Wagers[id].User2Consent = true;
+    }
+        
+
+    function acceptWager_Mediator(uint id) public {
+        require(msg.sender == Wagers[id].Mediator);
+        Wagers[id].MediatorConsent = true;
+    }
+
+    function settleWager(uint id, address winner) public {
+        require((winner == Wagers[id].User1)||(winner == Wagers[id].User2));
+
     }
 
 }
